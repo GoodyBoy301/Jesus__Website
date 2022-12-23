e235bf80c636f8a93bd68ba4923a564dab6d8f0c
Goodness Urama <goodyboy301@gmail.com>
2022-12-23 15:00:35 +0100

    first draft

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+require("dotenv").config();
+
 const express = require("express");
 const path = require("path");
 const preloadables = require("./preloadables");
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.port || 5000;
 
 app.use(express.static(path.join(__dirname, "public")));
 app.set("views", path.join(__dirname, "views"));
@@ -19,7 +21,7 @@ app.get("/:invalid", (req, res) => {
 });
 
 app.listen(port, () => {
-  //Stylishly log hostname and port
+  //stylishly log hostname and port
   console.log(
     `\x1b[32m Server listening at\x1b[0m`,
     `\x1b[4mhttp://localhost:${port}\x1b[0m`
@@ -27,3 +29,16 @@ app.listen(port, () => {
 });
 
 module.exports = app;
+
+// const Twitter = require("twitter");
+// const client = new Twitter({
+//   consumer_key: process.env.TWITTER_API_KEY,
+//   consumer_secret: process.env.TWITTER_SECRET,
+//   bearer_token: process.env.TWITTER_TOKEN,
+// });
+// client.get(
+//   "search/tweets", { q: "Goddid" },
+//   (error, tweets, response) => {
+//     console.log({ error, tweets, response });
+//   }
+// );
