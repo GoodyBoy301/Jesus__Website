b47f639dae8617ac5e372bae0ceda79c63fdeaa3
Goodness Urama <goodyboy301@gmail.com>
2023-01-03 21:30:55 +0100

    fix: twitter search

--- index.js
+++ index.js
@@ -3,17 +3,47 @@ require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const preloadables = require("./preloadables");
+const Twitter = require("twitter");
 const app = express();
 const port = process.env.port || 5000;
+let updates = [];
+
+const client = new Twitter({
+  consumer_key: process.env.TWITTER_API_KEY,
+  consumer_secret: process.env.TWITTER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_SECRET,
+});
+
+client.get(
+  "search/tweets",
+  { q: "#miraclenodeytirejesus" },
+  (error, tweets) => {
+    updates = tweets;
+  }
+);
 
 app.use(express.static(path.join(__dirname, "public")));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
 app.get("/", (req, res) => {
-  res.render("pages/home", {
-    preloadables,
-  });
+  client.get(
+    "search/tweets",
+    { q: "#miraclenodeytirejesus" },
+    (error, tweets) => {
+      updates = tweets;
+      res.render("pages/home", {
+        preloadables,
+        updates,
+        client,
+      });
+    }
+  );
+});
+
+app.get("/tweers", (req, res) => {
+  res.send(updates);
 });
 
 app.get("/:invalid", (req, res) => {
@@ -29,16 +59,3 @@ app.listen(port, () => {
 });
 
 module.exports = app;
-
-// const Twitter = require("twitter");
-// const client = new Twitter({
-//   consumer_key: process.env.TWITTER_API_KEY,
-//   consumer_secret: process.env.TWITTER_SECRET,
-//   bearer_token: process.env.TWITTER_TOKEN,
-// });
-// client.get(
-//   "search/tweets", { q: "Goddid" },
-//   (error, tweets, response) => {
-//     console.log({ error, tweets, response });
-//   }
-// );
