f45342f197d929b1284b7a88fa2715f154669cf6
goodness <goodness@revivar.app>
2023-01-26 19:53:14 +0100

    init

--- app/pages/Home/index.js
+++ app/pages/Home/index.js
@@ -1,25 +1,55 @@
-import Page from "classes/Page";
-import gsap from "gsap";
-import NormalizeWheel from "normalize-wheel";
-import Prefix from "prefix";
-import { clamp, lerp } from "utils/math";
+import LongPage from "classes/LongPage";
 import Reveal from "classes/Reveal";
+import NormalizeWheel from "normalize-wheel";
+import SplitType from "split-type";
+import { clamp, lerp, pixelToRem, snapTo } from "utils/math";
+import animate from "../../animations/@hamburger";
 
 export default class Home extends LongPage {
   constructor() {
     super({
       element: ".home",
       id: "home",
-      elements: {},
+      elements: {
+        wrapper: ".home__wrapper",
+        worksWrapper: ".home__works__wrapper",
+        parallaxElements: "[data-parallax]",
+      },
     });
   }
 
   /** Life Cycle */
   create() {
     super.create();
-    this.reCalculate();
+    this.reCalculate({ scroll: {} });
+    this.ham.animation = animate();
+    new SplitType(".home__work__title");
+    this.stats = new SplitType(".home__stats__number");
+    this.stats.chars.forEach((char) => {
+      char.setAttribute("before", Math.max(0, char.textContent - 1));
+      char.setAttribute("after", char.textContent);
+    });
+
+    new Reveal({
+      elements: {
+        work__title: ".home__work__title",
+        work__text: ".home__work__text",
+        stat: ".home__stats__number",
+        stat__desc: ".home__stats__description",
+        // work__image: ".home__work__image",
+        blackener: "h2 span",
+      },
+      animateIn: "fadeInScaleUp",
+      threshold: this.isMobile ? 1 : 0.2,
+      unobserve: this.isMobile ? true : false,
+    });
   }
-  reCalculate() {
-    super.reCalculate();
+
+  ham() {
+    const previousShowHam = this.showHam;
+    this.showHam = this.scroll.current > this.hamLowerLimit ? true : false;
+    if (this.showHam === previousShowHam) return;
+    if (this.showHam || this.isMobile) this.ham.animation.play();
+    else this.ham.animation.reverse();
   }
 }
